2692c815585aa5e4cf55da95546ea84c849b8667
Wilmai <artegraff.stile@gmail.com>
2020-07-21 20:03:51 -0400

    Creando Schema para nuestros datos y conectando postman
    Creando Schema para nuestros datos y conectando postman, dividiendo componentes y rutas.

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
 const router = express.Router();
+const Post = require('../models/Post');
 
 router.get('/', (req, res) => {
     res.send('We are on posts');
 });
 
+router.post('/', (req, res) => {
+    console.log(req.body);
+});
+
 module.exports = router;
+
